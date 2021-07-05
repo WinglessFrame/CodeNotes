d5eab94abaa8312fefa234940b76eab2eadb0c2a
WinglessFrame <gleb122002@gmail.com>
2021-07-05 12:17:28 +0300

    Added html inside an iframe reload on change. Added Monaco editor

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,20 @@
+import MonacoEditor from '@monaco-editor/react';
+
+const CodeEditor = () => {
+    return <MonacoEditor
+        height="500px"
+        language="javascript"
+        theme="dark"
+        options={{
+            wordWrap: 'on',
+            minimap: { enabled: false },
+            showUnused: false,
+            folding: false,
+            lineNumbersMinChars: 3,
+            fontSize: 16,
+            scrollBeyondLastLine: false
+        }}
+    />
+}
+
+export default CodeEditor
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/CodeEditor';
 
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -25,6 +25,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -36,7 +38,6 @@ const App = () => {
       },
     });
 
-    // setCode(result.outputFiles[0].text);
     iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
@@ -61,6 +62,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -68,8 +70,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} title="preview" />
     </div>
   );
 };
