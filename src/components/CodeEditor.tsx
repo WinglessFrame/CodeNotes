5850c6b7b6451527839e5bd7ad44c13edc4f6110
WinglessFrame <gleb122002@gmail.com>
2021-07-05 12:38:48 +0300

    Changed tabSize to 2

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -10,6 +10,8 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
         monacoEditor.onDidChangeModelContent(() => {
             onChange(getValue());
         })
+
+        monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
     }
 
     return <MonacoEditor
