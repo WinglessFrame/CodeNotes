40030bda2492790b0240143997d99f8d0d8f9583
WinglessFrame <gleb122002@gmail.com>
2021-07-05 12:36:39 +0300

    Added type defenition to editor update process

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 
 interface CodeEditorProps {
     initialValue: string
@@ -6,7 +6,7 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
-    const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+    const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
         monacoEditor.onDidChangeModelContent(() => {
             onChange(getValue());
         })
