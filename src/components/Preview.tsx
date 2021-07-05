660a6be492400071fe2873505a6ee76c3f821b29
WinglessFrame <gleb122002@gmail.com>
2021-07-05 15:44:49 +0300

    Refactor

---
+++ src/components/Preview.tsx
@@ -0,0 +1,46 @@
+
+import React, { useEffect, useRef } from 'react'
+
+interface PreviewProps {
+  code: string
+
+}
+
+const html = `
+<html>
+  <head></head>
+  <body>
+    <div id="root"></div>
+    <script>
+      window.addEventListener('message', (event) => {
+        try {
+          eval(event.data);
+        } catch (error) {
+          document.querySelector('#root').innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + error + '</div>'
+          console.error(error);
+        }
+      }, false);
+    </script>
+  </body>
+</html>
+`;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
+
+  useEffect(() => {
+    iframe.current.srcdoc = html
+    iframe.current.contentWindow.postMessage(code, '*');
+  }, [code])
+
+  return (
+    <iframe
+      ref={iframe}
+      sandbox="allow-scripts"
+      srcDoc={html}
+      title="preview"
+    />
+  )
+}
+
+export default Preview
--- src/index.tsx
+++ src/index.tsx
@@ -1,66 +1,18 @@
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/CodeEditor';
-
+import bundle from './bundler'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import Preview from './components/Preview';
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
-  const [input, setInput] = useState('');
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-  useEffect(() => {
-    startService();
-  }, []);
+  const [input, setInput] = useState<string>('');
+  const [code, setCode] = useState<string>('')
 
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    const output = await bundle(input)
+    setCode(output)
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch (error) {
-              document.querySelector('#root').innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + error + '</div>'
-              console.error(error);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
@@ -68,14 +20,10 @@ const App = () => {
         initialValue="document.querySelector('#root').innerHTML = '<h1>Hi!</h1>'"
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} title="preview" />
+      <Preview code={code} />
     </div>
   );
 };
