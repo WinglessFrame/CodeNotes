27c72d5914ec659c823b7ede89b48a1800b3aa4f
WinglessFrame <gleb122002@gmail.com>
2021-07-06 19:05:20 +0300

    Delered redundant variable

--- src/components/AddCell.tsx
+++ src/components/AddCell.tsx
@@ -10,10 +10,8 @@ interface AddCellProps {
 const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
   const { insertCellBefore } = useActions()
 
-  const lastOneStyle = nextCellId ? {} : { opacity: 1 }
-
   return (
-    <div className='add-cell' style={lastOneStyle}>
+    <div className='add-cell' style={{ opacity: nextCellId ? 0 : 1 }}>
       <button className='button is-rounded is-primary is-small' onClick={() => insertCellBefore(nextCellId, 'code')}>
         <span className='icon is-small'>
           <i className='fas fa-plus'></i>
