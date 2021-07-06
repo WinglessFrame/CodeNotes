5962e35302a759a9d154994eb34534b6cfd7a5ac
WinglessFrame <gleb122002@gmail.com>
2021-07-06 19:06:07 +0300

    Returned necessary variable

--- src/components/AddCell.tsx
+++ src/components/AddCell.tsx
@@ -10,8 +10,10 @@ interface AddCellProps {
 const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
   const { insertCellBefore } = useActions()
 
+  const lastOneStyle = nextCellId ? {} : { opacity: 1 }
+
   return (
-    <div className='add-cell' style={{ opacity: nextCellId ? 0 : 1 }}>
+    <div className='add-cell' style={lastOneStyle}>
       <button className='button is-rounded is-primary is-small' onClick={() => insertCellBefore(nextCellId, 'code')}>
         <span className='icon is-small'>
           <i className='fas fa-plus'></i>
