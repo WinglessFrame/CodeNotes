ef78906609d0c64206c70eb492306c8421fecdd7
WinglessFrame <gleb122002@gmail.com>
2021-07-07 14:42:58 +0300

    Fixed deps linter issue, modified useActions hook

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,8 +1,11 @@
+import { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../store';
 
 export const useActions = () => {
   const dispatch = useDispatch();
-  return bindActionCreators(actionCreators, dispatch);
+  return useMemo(() => {
+    return bindActionCreators(actionCreators, dispatch);
+  }, [dispatch]);
 };
