beaa21d77b8ff9edb047c4f5a1c080d53c6dc4ac
WinglessFrame <gleb122002@gmail.com>
2021-07-07 16:42:58 +0300

    Added show function that renders any data

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -19,7 +19,23 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells
     const orderedCells = order.map(id => data[id])
 
-    const codeCells = []
+    const codeCells = [
+      `
+        import _React from 'react'
+        import _ReactDOM from 'react-dom'
+        const show = (value) => {
+          const root = document.getElementById('root')
+          if (typeof value === 'object'){
+            if (value.$$typeof && value.props) {
+              _ReactDOM.render(value, root)
+            }else {
+              root.innerHTML = JSON.stringify(value)
+            }
+          } else
+          root.innerHTML = value
+        }
+      `
+    ]
     for (let c of orderedCells) {
       if (c.type === 'code') {
         codeCells.push(c.content)
